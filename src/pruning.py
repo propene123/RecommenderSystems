42611ba509b24ee2f8bba45b63453a6c8b7e4a10
Propene123 <benjamin.russell@durham.ac.uk>
2021-02-12 18:24:23 +0000

    more report stuff

--- src/pruning.py
+++ src/pruning.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-covid = pd.read_json('../data/org/covid.json', lines=True)
-full_bus = pd.read_csv('../data/pruned_bus.csv')
+bus = pd.read_csv('../data/pruned_bus.csv')
 
+foo = bus.loc[bus['review_count'] < 10]
 
-kek = pd.merge(covid, full_bus, how='inner', on='business_id')
-kek = kek.loc[:, ['business_id', 'delivery or takeout', 'Grubhub enabled']]
-
-kek.to_csv('pruned_covid.csv', index=False)
+bar = len(foo.index)/len(bus.index)
+bar *= 100
+print(f'{bar}%')
